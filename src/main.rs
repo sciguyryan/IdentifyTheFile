e7055fe8824b558e059337a46e1d02d0e7c04392
Ryan Jones-Ward <sciguyryan@gmail.com>
2024-07-23 12:48:17 +0100

    Now we can identify and handle substrings while building the reference list.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ use walkdir::WalkDir;
 const FILE_CHUNK_SIZE: usize = 10 * 1024 * 1024; // 10 MB
 
 const STRING_CHARS: &str =
-    " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-+_.$<>?=.,";
+    " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-+_.$<>?=.,/";
 const MIN_STRING_LENGTH: usize = 5;
-const MAX_STRING_LENGTH: usize = 150;
+const MAX_STRING_LENGTH: usize = 64;
 
 const HEADER_FRONT_SIZE: usize = 1024;
 
@@ -24,7 +24,7 @@ fn main() {
     let ref_chars: HashSet<u8> = STRING_CHARS.chars().map(|c| c as u8).collect();
 
     let mut is_first = true;
-    let mut reference_hashset = HashSet::new();
+    let mut hashsets: Vec<HashSet<String>> = Vec::new();
 
     for entry in WalkDir::new(file_dir) {
         let entry = entry.unwrap();
@@ -44,27 +44,40 @@ fn main() {
 
         // If we made it here then we have a valid file.
         let chunk = read_file_header_chunk(entry.path());
-        let strings_hashset = generate_file_hashset(&chunk, &ref_chars);
+        let new_hashset = generate_file_hashset(&chunk, &ref_chars);
+        hashsets.push(new_hashset);
+    }
 
-        if is_first {
-            reference_hashset = strings_hashset;
-            is_first = true;
-            continue;
-        }
+    if hashsets.is_empty() {
+        println!("No strings were found!");
+        return;
+    }
 
-        // We need to check for matching entries.
-        // TODO - a more thorough form of this would be to check if an entry is a substring
-        // TODO - of the other, trimming the string to a shorter length.
-        reference_hashset.retain(|s| strings_hashset.contains(s));
+    let reference_hashset = hashsets.remove(0);
+    let mut common_elements = HashSet::new();
 
-        if reference_hashset.is_empty() {
-            println!("No common strings identified.");
-            break;
+    // Iterate through the remaining sets
+    for set in hashsets {
+        for string in &set {
+            if reference_hashset.contains(string) {
+                // If the string is an exact match for one in the reference set.
+                common_elements.insert(string.clone());
+            } else {
+                // Check if the string is a substring of any string in the reference set.
+                for master_string in &reference_hashset {
+                    if master_string.contains(string) {
+                        common_elements.insert(string.clone());
+                        break;
+                    }
+                }
+            }
         }
     }
 
-    println!("{}", reference_hashset.len());
-    println!("{reference_hashset:?}");
+    println!(
+        "{} elements = {reference_hashset:?}",
+        reference_hashset.len()
+    );
 }
 
 fn read_file_header_chunk(file_path: &Path) -> Vec<u8> {
@@ -86,7 +99,7 @@ fn generate_file_hashset(bytes: &[u8], reference: &HashSet<u8>) -> HashSet<Strin
         if !reference.contains(byte) {
             // Start a new string sequence.
             if string_buffer.len() >= MIN_STRING_LENGTH {
-                string_map.insert(string_buffer);
+                string_map.insert(string_buffer.to_uppercase());
                 string_buffer = String::with_capacity(MAX_STRING_LENGTH);
             }
 
@@ -99,7 +112,7 @@ fn generate_file_hashset(bytes: &[u8], reference: &HashSet<u8>) -> HashSet<Strin
 
         // Is the string large enough that we must force a termination?
         if string_buffer.len() == MAX_STRING_LENGTH {
-            string_map.insert(string_buffer);
+            string_map.insert(string_buffer.to_uppercase());
             string_buffer = String::with_capacity(MAX_STRING_LENGTH);
         }
     }
