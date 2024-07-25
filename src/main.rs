b364deb56fdfc2e0d4f6c84cb2d59b21a7c137de
Ryan Jones-Ward <sciguyryan@gmail.com>
2024-07-25 13:00:05 +0100

    Implement common byte sequence identification.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{
-    cmp::min,
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     fs::File,
     io::{self, BufReader, Read},
     path::Path,
@@ -8,7 +7,7 @@ use std::{
 };
 
 use rayon::prelude::*;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 
 const FILE_CHUNK_SIZE: usize = 10 * 1024 * 1024; // 10 MB
 
@@ -17,67 +16,167 @@ const STRING_CHARS: [u8; 74] =
 const MIN_STRING_LENGTH: usize = 5;
 const MAX_STRING_LENGTH: usize = 128;
 
+const VERBOSE: bool = false;
+
 fn main() {
     let file_dir = "D:\\GitHub\\IdentifyTheFile\\samples";
-    let target_extension = "config";
+    let target_extension = "mkv";
 
     let ref_chars: HashSet<u8> = STRING_CHARS.iter().copied().collect();
 
-    let mut hashsets = Vec::new();
+    let mut initial_file_temp: Option<Vec<u8>> = None;
+    let mut common_byte_sequences = HashMap::new();
+    let mut common_strings = Vec::new();
+    let mut valid_sample_files = 0;
+
     for entry in WalkDir::new(file_dir) {
         let entry = entry.unwrap();
         if !entry.file_type().is_file() {
             continue;
         }
 
-        let ext = entry.path().extension();
-        let skip = match ext {
-            Some(str) => str != target_extension,
-            None => true,
-        };
-
-        if skip {
-            println!("Skipping file - {}", entry.path().to_string_lossy());
+        if !path_has_correct_extension(&entry, target_extension) {
+            if VERBOSE {
+                println!("Skipping file - {}", entry.path().to_string_lossy());
+            }
             continue;
         }
 
-        println!("Candidate file - {}", entry.path().to_string_lossy());
+        if VERBOSE {
+            println!("Candidate file - {}", entry.path().to_string_lossy());
+        }
+        valid_sample_files += 1;
 
         // If we made it here then we have a valid file.
         let chunk = read_file_header_chunk(entry.path()).expect("failed to read file");
+
         let new_hashset = generate_file_string_hashset(&chunk, &ref_chars);
+        common_strings.push(new_hashset);
 
-        hashsets.push(new_hashset);
-    }
+        // We want to avoid holding the header blocks of every file we scan,
+        // because that could have significant memory implications.
+        // Instead, we store the first file's header and then build the
+        // initial common sequences list from the first and second file headers.
+        // Files that follow those will be used to refine the sequences already
+        // identified.
+        if common_byte_sequences.is_empty() {
+            if let Some(file_1) = &initial_file_temp {
+                // Run the initial byte sequence scan.
+                common_byte_sequences = initial_common_byte_sequences_v1(file_1, &chunk);
 
-    if hashsets.is_empty() {
-        println!("No strings were found!");
-        return;
+                // Clear the superfluous from memory.
+                initial_file_temp = None;
+            } else {
+                initial_file_temp = Some(chunk);
+            }
+        } else {
+            refine_common_byte_sequences_v2(&chunk, &mut common_byte_sequences);
+        }
     }
 
     println!("-------------------------------------------------------");
-    println!("Starting string analysis (v2a)...");
+    println!("Valid sample files scanned {valid_sample_files}");
+    println!("-------------------------------------------------------");
+    println!("Matching positional byte sequences");
+    print_byte_sequence_matches(&common_byte_sequences);
+
+    if common_strings.is_empty() {
+        println!("No common strings were found!");
+    }
+
+    println!("-------------------------------------------------------");
+    println!("Starting string sieve (v2a)...");
     let before_v2a = Instant::now();
-    let common_strings_hashset_v2a = common_string_identification_v2a(&mut hashsets);
+    let common_strings_hashset_v2a = common_string_identification_v2a(&mut common_strings);
     println!("Elapsed time (v2a): {:.2?}", before_v2a.elapsed());
     println!("{}", common_strings_hashset_v2a.len());
     println!("------------------------------------------");
-
     let common_strings_hashset = common_strings_hashset_v2a;
-
-    println!("-------------------------------------------------------");
     println!("Final common strings = {common_strings_hashset:?}");
     println!("-------------------------------------------------------");
 
-    test_matching_files(
+    println!("Testing common string matches...");
+    test_matching_file_strings(
         file_dir,
         target_extension,
         &ref_chars,
         &common_strings_hashset,
     );
+
+    println!("Testing common byte sequence matches...");
+    test_matching_file_byte_sequences(file_dir, target_extension, &common_byte_sequences);
 }
 
-fn test_matching_files(
+fn path_has_correct_extension(entry: &DirEntry, ext: &str) -> bool {
+    match entry.path().extension() {
+        Some(str) => str == ext,
+        None => true,
+    }
+}
+
+fn print_byte_sequence_matches(sequences: &HashMap<usize, Vec<u8>>) {
+    let mut vec: Vec<(usize, Vec<u8>)> = sequences
+        .iter()
+        .map(|(index, m)| (*index, m.clone()))
+        .collect();
+    vec.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+    println!("{vec:?}");
+}
+
+fn test_matching_file_byte_sequences(
+    path: &str,
+    target_extension: &str,
+    sequences: &HashMap<usize, Vec<u8>>,
+) {
+    let mut all_success = true;
+    for entry in WalkDir::new(path) {
+        let entry = entry.unwrap();
+        if !entry.file_type().is_file() {
+            continue;
+        }
+
+        if !path_has_correct_extension(&entry, target_extension) {
+            continue;
+        }
+
+        let chunk = read_file_header_chunk(entry.path()).expect("failed to read file");
+
+        let mut matches = 0;
+        for (start, sequence) in sequences {
+            let end = *start + sequence.len();
+            if end > chunk.len() {
+                continue;
+            }
+
+            if sequence == &chunk[*start..end] {
+                matches += 1;
+            }
+        }
+
+        if VERBOSE {
+            println!("--------------------------------------");
+            println!("{}", entry.path().to_string_lossy());
+            println!("{} of {}", matches, sequences.len());
+        }
+
+        if matches == sequences.len() {
+            //println!("\x1b[92mSuccessful byte sequence matching!\x1b[0m");
+        } else {
+            //println!("\x1b[91mFailed byte sequence matching!\x1b[0m");
+            all_success = false;
+        }
+
+        all_success &= matches == sequences.len();
+    }
+
+    if all_success {
+        println!("\x1b[92mSuccessfully matched all applicable byte sequences!\x1b[0m");
+    } else {
+        println!("\x1b[91mFailed to match one or more byte sequences!\x1b[0m");
+    }
+}
+
+fn test_matching_file_strings(
     path: &str,
     target_extension: &str,
     ref_chars: &HashSet<u8>,
@@ -90,13 +189,7 @@ fn test_matching_files(
             continue;
         }
 
-        let ext = entry.path().extension();
-        let skip = match ext {
-            Some(str) => str != target_extension,
-            None => true,
-        };
-
-        if skip {
+        if !path_has_correct_extension(&entry, target_extension) {
             continue;
         }
 
@@ -113,22 +206,94 @@ fn test_matching_files(
             }
         }
 
-        println!("--------------------------------------");
-        println!("{}", entry.path().to_string_lossy());
-        println!("{} of {}", matches, common_strings.len());
+        if VERBOSE {
+            println!("--------------------------------------");
+            println!("{}", entry.path().to_string_lossy());
+            println!("{} of {}", matches, common_strings.len());
+        }
+
         if matches == common_strings.len() {
-            //println!("\x1b[92mSuccessful matching!\x1b[0m");
+            //println!("\x1b[92mSuccessful string matching!\x1b[0m");
         } else {
-            //println!("\x1b[91mFailed matching!\x1b[0m");
+            //println!("\x1b[91mFailed string matching!\x1b[0m");
             all_success = false;
         }
     }
 
     if all_success {
-        println!("\x1b[92mSuccessfully matched all applicable items!\x1b[0m");
+        println!("\x1b[92mSuccessfully matched all common strings!\x1b[0m");
     } else {
-        println!("\x1b[91mFailed to match one or more applicable items\x1b[0m");
+        println!("\x1b[91mFailed to match one or more common strings!\x1b[0m");
     }
+}
+
+fn refine_common_byte_sequences_v2(
+    file_bytes: &[u8],
+    common_byte_sequences: &mut HashMap<usize, Vec<u8>>,
+) {
+    let mut refined_sequence = HashMap::with_capacity(common_byte_sequences.len());
+
+    for (index, sequence) in common_byte_sequences.iter() {
+        // If the final index would fall outside the bounds of the
+        // chunk then read to the end of the chunk instead.
+        let segment_read_length = *index + sequence.len().min(file_bytes.len());
+
+        // We can be certain that this will always fall within bounds.
+        let new_segment = &file_bytes[*index..segment_read_length];
+
+        // Fast path exit for a complete match.
+        if sequence == new_segment {
+            refined_sequence.insert(*index, sequence.clone());
+            continue;
+        }
+
+        // Check to see if we can find a valid sub-match. If so, we'll retain that instead.
+        let mut new_length = segment_read_length;
+        for i in 0..segment_read_length {
+            if sequence[i] != new_segment[i] {
+                new_length = i;
+                break;
+            }
+        }
+
+        if new_length > 0 {
+            refined_sequence.insert(*index, sequence[..new_length].to_vec());
+        }
+    }
+
+    *common_byte_sequences = refined_sequence;
+}
+
+fn initial_common_byte_sequences_v1(
+    file_1_bytes: &[u8],
+    file_2_bytes: &[u8],
+) -> HashMap<usize, Vec<u8>> {
+    let mut common_byte_sequences = HashMap::new();
+
+    let mut sequence_start = 0;
+    let mut sequence_end;
+    let mut in_sequence = false;
+    for (i, b) in file_1_bytes.iter().enumerate() {
+        if i < file_2_bytes.len() && file_2_bytes[i] == *b {
+            // Indicate the start of the matching sequence, if we aren't already
+            // within a sequence.
+            if !in_sequence {
+                sequence_start = i;
+                in_sequence = true;
+            }
+
+            continue;
+        }
+
+        // We have reached the end of the matching sequence.
+        sequence_end = i;
+        in_sequence = false;
+
+        let sequence = file_1_bytes[sequence_start..sequence_end].to_vec();
+        common_byte_sequences.insert(sequence_start, sequence);
+    }
+
+    common_byte_sequences
 }
 
 fn common_string_identification_v2a(hashsets: &mut Vec<HashSet<String>>) -> HashSet<String> {
@@ -161,32 +326,50 @@ fn common_string_identification_v2a(hashsets: &mut Vec<HashSet<String>>) -> Hash
         set.retain(|s| !temp_set.contains(s));
 
         // Parallel iterate over the entries.
-        temp_set.par_extend(common_strings_hashset.par_iter().filter_map(|ref_string| {
-            let mut largest_match = "";
+        temp_set.par_extend(
+            common_strings_hashset
+                .par_iter()
+                .filter_map(|common_string| {
+                    let mut largest_match = "";
 
-            for string in &set {
-                // Are we able to match a substring between the reference and new string?
-                if let Some(s) = largest_common_substring(string, ref_string) {
-                    // Check if this is the largest match we've seen so far.
-                    if s.len() > largest_match.len() {
-                        largest_match = s;
+                    for string in &set {
+                        // Are we able to match a substring between the reference and new string?
+                        if let Some(s) = largest_common_substring(string, common_string) {
+                            // Check if this is the largest match we've seen so far.
+                            if s.len() > largest_match.len() {
+                                largest_match = s;
+                            }
+                        }
                     }
-                }
-            }
 
-            // Only insert if a match was found.
-            if !largest_match.is_empty() {
-                Some(largest_match.to_string())
-            } else {
-                None
-            }
-        }));
+                    // Only insert if a match was found.
+                    if !largest_match.is_empty() {
+                        Some(largest_match.to_string())
+                    } else {
+                        None
+                    }
+                }),
+        );
 
         // Update the common hashmap to reflect the new changes.
         common_strings_hashset = temp_set;
     }
 
-    common_strings_hashset
+    // There is one final step here, we do not want to retain any items that are
+    // simply substrings of larger items.
+    // They won't add anything unique.
+    let mut final_hashset = HashSet::new();
+
+    for item in &common_strings_hashset {
+        if !common_strings_hashset
+            .iter()
+            .any(|other| other != item && other.contains(item))
+        {
+            final_hashset.insert(item.clone());
+        }
+    }
+
+    final_hashset
 }
 
 fn all_substrings_over_min_size(string: &str) -> Vec<&str> {
@@ -213,7 +396,7 @@ fn largest_common_substring<'a>(str_1: &'a str, str_2: &str) -> Option<&'a str> 
 fn read_file_header_chunk(file_path: &Path) -> io::Result<Vec<u8>> {
     let file = File::open(file_path)?;
     let filesize = file.metadata()?.len() as usize;
-    let read_size = min(filesize, FILE_CHUNK_SIZE);
+    let read_size = filesize.min(FILE_CHUNK_SIZE);
     let mut buf_reader = BufReader::new(file);
     let mut buffer = vec![0; read_size];
     buf_reader.read_exact(&mut buffer)?;
