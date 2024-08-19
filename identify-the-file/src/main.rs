f940ecce3c75a0a7057454ec34ffc98db9cd8271
Ryan Jones-Ward <sciguyryan@gmail.com>
2024-08-19 11:19:50 +0100

    Restructure the crate into two sections - the core library and the CLI implementation.

--- identify-the-file/src/main.rs
+++ identify-the-file/src/main.rs
@@ -1,19 +1,13 @@
-mod file_point_calculator;
-mod file_processor;
-mod pattern;
-mod pattern_handler;
-#[cfg(test)]
-mod test_utils;
-mod utils;
+#![crate_name = "identify_the_file"]
 
 use clap::{Parser, Subcommand};
-use file_point_calculator::FilePointCalculator;
-use pattern_handler::PatternHandler;
+use itf_core::{
+    file_point_calculator::FilePointCalculator, file_processor, pattern::Pattern,
+    pattern_handler::PatternHandler, utils,
+};
 use prettytable::{Cell, Row, Table};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::{env, path::PathBuf};
-
-use pattern::Pattern;
 
 #[derive(Parser)]
 #[command(
@@ -403,136 +397,6 @@ fn process_pattern_command(cmd: &Commands) {
         } else {
             let json = serde_json::to_string(&pattern).unwrap();
             println!("{json}");
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests_pattern {
-    use std::{fs, path::PathBuf};
-
-    use crate::{
-        built_pattern_handler, match_patterns, pattern::Pattern, pattern_handler::PatternHandler,
-        test_utils, utils,
-    };
-
-    struct TestEntry {
-        #[allow(unused)]
-        path: FileDropper,
-        pub new_pattern: Pattern,
-        pub pattern_handler: PatternHandler,
-        pub test_dir: String,
-    }
-
-    impl TestEntry {
-        pub fn new(test_id: usize) -> Self {
-            let id = test_id.to_string();
-
-            // Build a pattern from the sample files.
-            let (path, pattern) = Self::build_test("matching", &id);
-
-            let test_dir = test_utils::test_path_builder("matching", &id);
-
-            // Read all of the test patterns.
-            let pattern_handler = built_pattern_handler(&test_dir, "");
-            assert!(
-                !pattern_handler.is_empty(),
-                "pattern handler didn't load the pattern files"
-            );
-
-            Self {
-                path,
-                new_pattern: pattern,
-                pattern_handler,
-                test_dir,
-            }
-        }
-
-        fn build_test(test_type: &str, test_id: &str) -> (FileDropper, Pattern) {
-            let test_dir = test_utils::test_path_builder(test_type, test_id);
-
-            let mut pattern = Pattern::new("valid", "test", vec!["test".to_string()], vec![]);
-            pattern.build_patterns_from_data(&test_dir, "test", true, true, true);
-
-            // Write the pattern file.
-            let path = pattern.write(&test_dir).expect("failed to write test file");
-
-            (FileDropper { path }, pattern)
-        }
-
-        fn get_test_file(&self, id: usize) -> String {
-            self.get_test_file_of_type(id, "test")
-        }
-
-        fn get_test_file_of_type(&self, id: usize, extension: &str) -> String {
-            let files = utils::list_files_of_type(&self.test_dir, extension);
-            files.get(id).expect("failed to find test file").to_owned()
-        }
-    }
-
-    #[test]
-    fn test_matching_1() {
-        let test = TestEntry::new(1);
-
-        // There should only ever be a single result.
-        let results = match_patterns(&test.pattern_handler, &test.get_test_file(0));
-        assert_eq!(results.len(), 1);
-
-        let first_result = results.first().unwrap();
-        assert_eq!(first_result.uuid, test.new_pattern.type_data.uuid);
-        assert_eq!(first_result.percentage, 100.0);
-    }
-
-    #[test]
-    fn test_matching_2() {
-        // The directory contains another pattern file that is not a match due to
-        // a byte sequence mismatch.
-        let test = TestEntry::new(2);
-
-        // There should only ever be a single result.
-        let results = match_patterns(&test.pattern_handler, &test.get_test_file(0));
-        assert_eq!(results.len(), 1);
-
-        let top_match = results.first().unwrap();
-        assert_eq!(top_match.uuid, test.new_pattern.type_data.uuid);
-        assert_eq!(top_match.percentage, 100.0);
-    }
-
-    #[test]
-    fn test_matching_3() {
-        // The directory contains another pattern file that is not a match due to
-        // a byte sequence mismatch.
-        let test = TestEntry::new(3);
-
-        // There should be two results, the created pattern being the top one.
-        // This is because the existing pattern is a "less perfect" match.
-        let results = match_patterns(&test.pattern_handler, &test.get_test_file(0));
-        assert_eq!(results.len(), 2);
-
-        let top_match = results.first().unwrap();
-        assert_eq!(top_match.uuid, test.new_pattern.type_data.uuid);
-        assert_eq!(top_match.percentage, 100.0);
-    }
-
-    #[test]
-    fn test_matching_4() {
-        let test = TestEntry::new(4);
-
-        // There should be no matches for the target file since it is
-        // fundamentally different than the defined pattern file.
-        let results = match_patterns(&test.pattern_handler, &test.get_test_file_of_type(0, "abc"));
-        assert_eq!(results.len(), 0);
-    }
-
-    /// Automatically delete a file after a test has been completed.
-    /// Use with care! Files go bye-bye!
-    struct FileDropper {
-        path: PathBuf,
-    }
-
-    impl Drop for FileDropper {
-        fn drop(&mut self) {
-            _ = fs::remove_file(&self.path);
         }
     }
 }
