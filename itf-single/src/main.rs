90b65a9cb7c19fee6455b45f6a92844889ec03e4
Ryan Jones-Ward <sciguyryan@gmail.com>
2024-08-19 12:20:37 +0100

    Work on the placeholder for the singe file pattern identification prototype.

--- itf-single/src/main.rs
+++ itf-single/src/main.rs
@@ -1,3 +1,16 @@
 #![crate_name = "itf_single"]
 
-fn main() {}
+// This pattern block will be patched to contain the JSON data.
+const PLACEHOLDER: u8 = 35;
+const PATTERN_BLOCK_SIZE: usize = 64 * 1024;
+static PATTERN: [u8; PATTERN_BLOCK_SIZE] = [PLACEHOLDER; PATTERN_BLOCK_SIZE];
+
+fn main() {
+    if let Ok(str) = String::from_utf8(PATTERN.to_vec()) {
+        println!("{}", &str[0..16]);
+    } else {
+        eprintln!("The included pattern data was corrupted.");
+    }
+
+    //from_simd_json_str
+}
